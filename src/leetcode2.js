1b43a9ac317b1851cc77da8180970c52a08eb061
wishtony <vishnutoanother@gmail.com>
2023-12-24 10:28:22 +0530

    commit

--- src/leetcode2.js
+++ src/leetcode2.js
@@ -10,25 +10,25 @@
 // Explanation: a = "text" and b = "book". a has 1 vowel whereas b has 2. Therefore, they are not alike.
 // Notice that the vowel o is counted twice.
 
-var halvesAreAlike = function(s) {
-    // Split the string into two halves
-    const halfLength = s.length / 2;
-    const a = s.slice(0, halfLength);
-    const b = s.slice(halfLength);
+var halvesAreAlike = function (s) {
+  // Split the string into two halves
+  const halfLength = s.length / 2;
+  const a = s.slice(0, halfLength);
+  const b = s.slice(halfLength);
 
-    // Helper function to count vowels in a string
-    const countVowels = (str) => {
-        const vowels = "aeiouAEIOU";
-        let count = 0;
-        for (const char of str) {
-            if (vowels.includes(char)) {
-                count++;
-            }
-        }
-        return count;
-    };
-    // Compare vowel counts in the two halves
-    return countVowels(a) === countVowels(b);
+  // Helper function to count vowels in a string
+  const countVowels = (str) => {
+    const vowels = "aeiouAEIOU";
+    let count = 0;
+    for (const char of str) {
+      if (vowels.includes(char)) {
+        count++;
+      }
+    }
+    return count;
+  };
+  // Compare vowel counts in the two halves
+  return countVowels(a) === countVowels(b);
 };
 
 // Example usage:
