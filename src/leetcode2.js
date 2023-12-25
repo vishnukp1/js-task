f28a18d423c1e8103010a475bb12ed3e1a4390a5
wishtony <vishnutoanother@gmail.com>
2023-12-25 08:51:57 +0530

    update commit

--- src/leetcode2.js
+++ src/leetcode2.js
@@ -14,7 +14,7 @@ var halvesAreAlike = function (s) {
   // Split the string into two halves
   const halfLength = s.length / 2;
   const a = s.slice(0, halfLength);
-  const b = s.slice(halfLength);
+  const b = s.slice(halfLength); 
 
   // Helper function to count vowels in a string
   const countVowels = (str) => {
