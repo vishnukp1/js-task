707a327fb8a46ad7e5bfe468764d81e88fef5d40
wishtony <vishnutoanother@gmail.com>
2023-12-28 13:23:25 +0530

    COOMIT

--- src/leetcode8.js
+++ src/leetcode8.js
@@ -17,32 +17,32 @@
 
 // Input: s = "1111"
 // Output: 2
-// Explanation: You need two operations to reach "0101" or "1010". 
+// Explanation: You need two operations to reach "0101" or "1010".
 
 function minOperations(s) {
-    function countOperations(startChar) {
-        let operations = 0;
-        for (let i = 0; i < s.length; i++) {
-            if (s[i] !== startChar) {
-                operations++;
-            }
-            startChar = startChar === '1' ? '0' : '1';
-        }
-        return operations;
+  function countOperations(startChar) {
+    let operations = 0;
+    for (let i = 0; i < s.length; i++) {
+      if (s[i] !== startChar) {
+        operations++;
+      }
+      startChar = startChar === "1" ? "0" : "1";
     }
+    return operations;
+  }
 
-    const operationsWith0 = countOperations('0');
-    const operationsWith1 = countOperations('1');
-    
-    return Math.min(operationsWith0, operationsWith1);
+  const operationsWith0 = countOperations("0");
+  const operationsWith1 = countOperations("1");
+
+  return Math.min(operationsWith0, operationsWith1);
 }
 
 // Example usage:
 const s1 = "0100";
-console.log(minOperations(s1));  // Output: 1
+console.log(minOperations(s1)); // Output: 1
 
 const s2 = "10";
-console.log(minOperations(s2));  // Output: 0
+console.log(minOperations(s2)); // Output: 0
 
 const s3 = "1111";
-console.log(minOperations(s3));  // Output: 2
+console.log(minOperations(s3)); // Output: 2
